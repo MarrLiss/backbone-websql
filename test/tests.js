1660c2129d41170b713b7ddcb151892c12168869
Peter Rust <peter@cornerstonenw.com>
2013-04-23 12:02:38 -0700

    Made tests easier to read using idiomatic node JS
    - switched to single callbacks
    - using async instead of _.after()

--- test/tests.js
+++ test/tests.js
@@ -1,147 +1,140 @@
 (function(Backbone, _) {
 var assert = chai.assert;
 
-var db = openDatabase("bb-websql-tests", "", "Backbone Websql Tests", 1024*1024);
+var db = openDatabase('bb-websql-tests', '', 'Backbone Websql Tests', 1024*1024);
 
 var ThingModel = Backbone.Model.extend({
-  store: new WebSQLStore(db, "things")
+  'store': new WebSQLStore(db, 'things')
 });
 var ThingCollection = Backbone.Collection.extend({
-  model: ThingModel,
-  store: ThingModel.prototype.store
+  'model': ThingModel,
+  'store': ThingModel.prototype.store
 });
 
 describe('Backbone.WebSQL', function() {
 
   afterEach(teardown);
 
-  it("should save/load by id", function(done){
+  it('should save/load by id', function(done) {
     var model = new ThingModel();
     assert(!model.id);
     model.set({'name': 'some name'})
 
-    model.save({}, {
-      success: function(m, resp){
-        assert(model.id);
-        loadTest();
-      },
-      error: done.bind(null, 'saving failed')
-    });
-
-    function loadTest(){
-      var loadModel = new ThingModel({id: model.id});
-      loadModel.fetch({  
-        success: function(){
-          assert.deepEqual(loadModel.toJSON(), model.toJSON());
-          done();
-        },
-        error: done.bind(null, 'loading failed')
-      });
-    }
+    model.save(null, cb(function(err) {
+      if (err) return done('saving failed');
+      assert(model.id);
+      
+      var loadModel = new ThingModel({'id': model.id});
+      loadModel.fetch(cb(function(err) {
+        if (err) return done('loading failed');
+        assert.deepEqual(loadModel.toJSON(), model.toJSON());
+        done();
+      }));
+    }));
   });
 
-  it("should not save apiid ", function(done){
-    var model = new ThingModel({name: "some thing"});
-    model.set({apiid: (new Date()).getTime()});
+  it('should not save apiid ', function(done) {
+    var model = new ThingModel({'name': 'some thing'});
+    model.set({'apiid': Date.now()});
 
-    model.save();
-    assert(model.id);
-
-    var model2 = new ThingModel({id: model.id});
-    model2.fetch({
-      success: function(){
+    model.save(null, cb(function(err) {
+      if (err) return done(err);
+      assert(model.id);
+      
+      var model2 = new ThingModel({'id': model.id});
+      model2.fetch(cb(function(err) {
+        if (err) return done('model2.fetch() failed');
         assert.equal(model2.get('name'), model.get('name'));
         assert(!model2.get('apiid'), 'should not save apiid');
         done();
-      },
-      error: done.bind(null, 'model2.fetch() failed')
-    });
+      }));
+    }));
   });
 
   describe('Collection.fetch()', function() {
-    it("should populate the collection", function(done){
+    it('should populate the collection', function(done) {
       var coll = new ThingCollection();
-      var model = new ThingModel({name: "some thing"});
-      model.save(null, {
-        success: function(){
-          coll.fetch({
-            success: function(){
-              assert.equal(coll.length, 1);
-              done();
-            },
-            error: done.bind(null, 'coll.fetch() failed')
-          });
-        },
-        error: done.bind(null, 'model.save() failed')
-      });
+      var model = new ThingModel({name: 'some thing'});
+      
+      model.save(null, cb(function(err) {
+        if (err) return done('model.save() failed');
+
+        coll.fetch(cb(function(err) {
+          if (err) return done('coll.fetch() failed');
+          assert.equal(coll.length, 1);
+          done();
+        }));
+      }));
     });
 
-    it("should work, even if Model.fetch() is called immediately after it", function(done) {
+    it('should work, even if Model.fetch() is called immediately after it', function(done) {
       var coll = new ThingCollection();
-      var model = new ThingModel({name: "some thing"});
-      model.save(null, {
-        success: function(){
-          var model2 = new ThingModel({id: model.id});
-          var success = _.after(2, function() {
-            assert.equal(coll.length, 1);
-            assert.equal(model2.get("name"), "some thing");
-            done();
-          });
-          coll.fetch({
-            success: success,
-            error: done.bind(null, 'coll.fetch() failed')
-          });
-          model2.fetch({
-            success: success,
-            error: done.bind(null, 'model.fetch() failed')
-          });
-        },
-        error: done.bind(null, 'model.save() failed')
-      });
+      var model = new ThingModel({name: 'some thing'});
+      
+      model.save(null, cb(function(err) {
+        if (err) return done('model.save() failed');
+
+        var model2 = new ThingModel({'id': model.id});
+        async.parallel([
+          function(callback) { coll.fetch(cb(callback)); },
+          function(callback) { model2.fetch(cb(callback)); }
+        ], function(err) {
+          if (err) return done('fetch() failed');
+          assert.equal(coll.length, 1);
+          assert.equal(model2.get('name'), 'some thing');
+          done();
+        });
+      }));
     });
   });
 
   describe('Model.fetch()', function() {
-    it("should update a single model", function(done) {
-      var model = new ThingModel({name: "some thing"});
-      model.save(null, {
-        success: function(){
-          var model2 = new ThingModel({id: model.id});
-          model2.fetch({
-            success: function() {
-              assert.equal(model2.get("name"), "some thing");
-              done();
-            },
-            error: done.bind(null, 'model2.fetch() failed')
-          })
-        },
-        error: done.bind(null, 'model.save() failed')
-      });
+    it('should update a single model', function(done) {
+      var model = new ThingModel({name: 'some thing'});
+      model.save(null, cb(function(err) {
+        if (err) return done('model.save() failed');
+
+        var model2 = new ThingModel({id: model.id});
+        model2.fetch(cb(function(err) {
+          if (err) return done('model2.fetch() failed');
+
+          assert.equal(model2.get('name'), 'some thing');
+          done();
+        }));
+      }));
     });
   });
-
 });
 
 function teardown(done) {
   var coll = new ThingCollection();
-  coll.fetch({
-    success: function(){
-      var success = _.after(coll.length, function(){
-        done();
-      });
-      _.invoke(_.clone(coll.models), 'destroy', {
-        success: success,
-        error: done.bind(null, 'model.destroy() failed')
-      });
-    },
-    error: done.bind(null, 'teardown fetch failed')
-  });
+  coll.fetch(cb(function(err) {
+    if (err) return done('teardown fetch failed');
+    var models = _.clone(coll.models);
+    async.forEach(models, destroy, function(err) {
+      if (err) return done('model.destroy() failed');
+      done();
+    });
+
+    function destroy(model, callback) {
+      model.destroy(cb(callback));
+    }
+  }));
 }
 
-function error(msg, model, resp){
-  start();
-  console.error(resp);
-  ok(false, msg);
+function cb(options, callback) {
+  if (typeof options == 'function') {
+    callback = options;
+    options = {};
+  }
+  if (!options) options = {};
+  options.success = function() {
+    callback.apply(null, [null].concat(arguments));
+  };
+  options.error = function(model, err) {
+    callback.apply(null, [err || true].concat(arguments));
+  };
+  return options;
 }
 
 })(Backbone, _);
